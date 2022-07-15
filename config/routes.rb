d30ecfc273722a2625db65e906d04cb713ebbecc
Kevin Hang <herezy2006@gmail.com>
2022-07-15 23:12:01 +0200

    routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %i[create show]
+    resource :session, only: %i[create destroy]
+
+    resources :movies, only: %i[show index]
+    resources :genres, only: %i[show index]
+    resources :profiles, only: %i[create destroy show]
+    resources :lists, only: %i[create destroy show]
+    resources :list_movies, only: %i[create destroy show]
+  end
 end
