98815df3826771267b87bdc3176b5f412b1b896b
Kevin Hang <herezy2006@gmail.com>
2022-07-16 00:47:20 +0200

    seeds + front(react/redux)

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = props => (
+    <footer className="footer-main">
+        <div className="footer-left">
+            <p className="footer-text">Mastering The Sharingans.</p>
+        </div>
+        <div className="footer-right">
+            <a className="footer-link" target="_blank" rel="noreferrer" href="https://github.com/Haloriginee"><div className="github-icon"></div></a>
+            <a className="footer-link" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/phoking/"><div className="linkedin-icon"></div></a>
+        </div>
+    </footer>
+)
+
+export default Footer;
